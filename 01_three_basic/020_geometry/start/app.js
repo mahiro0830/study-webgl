8da8a338ef18896e74342e416d6112e3b68511c8
峯岸真弘 <shishisus4@gmail.com>
2024-01-04 21:26:09 +0900

    TorusGeometryを使用

--- 01_three_basic/020_geometry/start/app.js
+++ 01_three_basic/020_geometry/start/app.js
@@ -16,12 +16,12 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const geometry = new THREE.TorusGeometry( 10, 3, 100, 100 );
+const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+camera.position.z = 50;
 
 let i = 0;
 function animate() {
